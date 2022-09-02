e818d6a1b63b9826e687e267f5bddd4cc6925333
Anna Luz Fisher <annaluzfisher@gmail.com>
2022-09-01 18:39:47 -0700

    adds views and routes and session controller

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,34 @@
+class Api::SessionsController < ApplicationController
+
+  def show
+    # debugger
+    @user = User.find_by(session_token: session[:session_token])
+  if   @user
+   render "/api/users/show"
+    else
+      render json: { user: nil }
+    end
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:email],params[:password])
+
+    if @user
+      login!(@user)
+      render "/api/users/show"
+    else
+      render json: { errors: ['The provided credentials were invalid.']}, status: 422
+    end
+  end
+
+  def destroy
+      @user = User.find_by(session_token: session[:session_token])
+  if   @user
+      logout
+      render json: { message: 'success' }
+    else
+       render json: { message: 'fail' }
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [ :create ]
+   resource :session, only: [ :show, :create, :destory ]
+   get '*path', to: 'static_pages#frontend_index'
+  end
 end
