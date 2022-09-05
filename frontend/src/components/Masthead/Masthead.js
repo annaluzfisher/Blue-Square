31ec169a9bf5452b21722d31734193b30f50e438
Anna Luz Fisher <annaluzfisher@gmail.com>
2022-09-05 16:42:06 -0700

    Adds elements to masthead

--- frontend/src/components/Masthead/Masthead.js
+++ frontend/src/components/Masthead/Masthead.js
@@ -1,16 +1,21 @@
 import './masthead.css'
-
+import Button from '../Buttons/Button/Button'
 
 function Masthead({promoTitle,imgUrl}) {
   return (
     <div className="masthead">
-      <div className='overlay'></div>
+      <div className="overlay"></div>
       <img src={process.env.PUBLIC_URL + imgUrl} />
-      <div className='masthead-content-wrapper' >
-      <h1 className="page-title">{promoTitle}</h1>
-      <div className='promo-details'>
-          Save Up to 30% Off Selected Equipment and 25% Off Selected Apparel Valid through 10/5/22
-      </div>
+      <div className="masthead-content-wrapper">
+        <h1 className="page-title">{promoTitle}</h1>
+        <div className="promo-details">
+          <span>Save Up to 30% Off Selected Equipment</span>
+          <span>and 25% Off Selected Apparel</span>
+          <span>Valid through 10/5/22</span>
+        </div>
+        <div className='buttons-contaianer'>
+        <Button name={'SHOP WINTER GEAR'} /> <Button name={"SHOP SKI & RIDE"}/> <Button name={'SHOP MOUNTAIN'} />
+        </div>
       </div>
     </div>
   );
