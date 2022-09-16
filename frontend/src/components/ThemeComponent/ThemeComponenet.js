5e14365b8a44943c34f8e2779e667430576fda37
Anna Luz Fisher <annaluzfisher@gmail.com>
2022-09-16 09:29:21 -0700

    adds theme flexibility

--- frontend/src/components/ThemeComponent/ThemeComponenet.js
+++ frontend/src/components/ThemeComponent/ThemeComponenet.js
@@ -1,58 +1,58 @@
-import LazyImage from './LazyImage';
-import './themecomponent.css'
-import {Link} from 'react-router-dom'
+import LazyImage from "./LazyImage";
+import "./themecomponent.css";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { fetchCollections, getCollections } from "../../store/collections";
 
-function ThemeComponenet({collectionIds}) {
-
+function ThemeComponenet({ collectionIds }) {
   const dispatch = useDispatch();
   const storeCollections = useSelector(getCollections());
   const [collections, setCollections] = useState(storeCollections);
-  // const [categoryIds, setCategoryIds] = useState([])
-  // const categoryImages = useSelector()
 
-  // const storeCategory = useSelector(getCategory(categoryId));
-  // const [category, setCategory] = useState();
-
+  const [campingId, setCampingId] = useState(1);
+  const [equpmentId, setEquipmentId] = useState(1);
+  const [winterId, setWinterId] = useState(1);
+  const [climbingId, setClimbingId] = useState(1);
   useEffect(() => {
-    if (!collections){
-    dispatch(fetchCollections());
+    if (!collections) {
+      dispatch(fetchCollections());
     }
     setCollections(storeCollections.collections);
-
   }, [collectionIds]);
-    
- useEffect(() => {
-   if (collections){
 
-   }
-
- }, [collectionIds]);
-
+  useEffect(() => {
+    if (collections) {
+      Object.values(collections).map((collection) => {
+        if (collection.name === "Camping") setCampingId(collection.id);
+        if (collection.name === "Winter") setWinterId(collection.id);
+        if (collection.name === "Equipment") setEquipmentId(collection.id);
+        if (collection.name === "Climbing") setClimbingId(collection.id);
+      });
+    }
+  }, [collections]);
 
   return (
     <div className="theme-component">
       <LazyImage
         imageUrl="/BannerImages/image6.jpg"
         text={"Climbing"}
-        collectionId={2}
+        collectionId={climbingId}
       />
       <LazyImage
         imageUrl="/BannerImages/camping.jpg"
         text={"CAMPING"}
-        collectionId={6}
+        collectionId={campingId}
       />
       <LazyImage
         imageUrl="/BannerImages/snow.jpg"
         text={"SNOW"}
-        collectionId={1}
+        collectionId={winterId}
       />
       <LazyImage
         imageUrl="/BannerImages/accessories.jpg"
         text={"EQUIPMENT"}
-        collectionId={6}
+        collectionId={equpmentId}
       />
     </div>
   );
