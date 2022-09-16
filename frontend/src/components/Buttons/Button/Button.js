39dd0bc069d63de3c99776dad7d3e39d94deb170
Anna Luz Fisher <annaluzfisher@gmail.com>
2022-09-16 10:17:35 -0700

    button js

--- frontend/src/components/Buttons/Button/Button.js
+++ frontend/src/components/Buttons/Button/Button.js
@@ -1,49 +1,40 @@
-import './button.css'
-import { Link } from 'react-router-dom'
-import { orange } from '../../../Util/Styles'
-import { useDispatch, useSelector } from 'react-redux'
-import { toggleModal } from '../../../store/ui'
+import "./button.css";
+import { Link } from "react-router-dom";
+import { orange } from "../../../Util/Styles";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleModal } from "../../../store/ui";
 //
 //primary interpolared iojt button class
 
-function Button({ localPath = "/", name, type, color = '#CD4C1D' }) {
+function Button({
+  localPath = "/",
+  name,
+  type,
+  color = "#CD4C1D",
+  primary = "primary",
+}) {
   const dispatch = useDispatch();
   const ADDED_ID = 4;
-  const modals = useSelector((state) => Object.values(state.ui.modals))
-  const handleClick = ()=>{
-
-      modals.map((modal) => {
-      if (modal.visible) dispatch(toggleModal(modal.id))
-    })
-  }
- 
-  
-
+  const modals = useSelector((state) => Object.values(state.ui.modals));
+  const handleClick = () => {
+    modals.map((modal) => {
+      if (modal.visible) dispatch(toggleModal(modal.id));
+    });
+  };
 
   if (type === "submit") {
     return (
       <div>
-        <input
-          className="button"
-          style={{ backgroundColor: color }}
-          type="submit"
-          value={name}
-        />
+        <input className={`button ${primary}`} type="submit" value={name} />
       </div>
     );
   } else {
     return (
       <Link to={localPath}>
-        <div
-          className="button"
-          style={{ backgroundColor: color }}
-          onClick={handleClick}
-        >
-          {name}{" "}
-        </div>
+        <div className={`button ${primary}`}>{name} </div>
       </Link>
     );
-  } 
+  }
 }
 
 export default Button;
